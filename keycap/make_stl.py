bd8e931a6725c4c8d55dcf540b0f05a642fd2189
Firstname Lastname <Somone@someplace.com>
2014-12-19 17:50:31 +0000

    STL generator updates to not make everything since it takes ages.

--- keycap/make_stl.py
+++ keycap/make_stl.py
@@ -3,31 +3,41 @@
 import os
 from subprocess import check_call
 
-stems = ['ALPS', 'CHERRYMX']
-profiles = ['DSA', 'DCS']
+stems = {
+#         'cherrymx': 1,
+         'alps': 2,
+        }
+
+profiles = {
+#            'dcs': 1,
+            'dsa': 2,
+           }
+
 bools = ['bump']
-sizes = [1, 1.5, 2]
 
-os.makedirs('stl')
+sizes = [
+         1.0,
+         1.5,
+         2.0,
+        ]
 
-cmds = []
-for s, stem in enumerate(stems):
-    stem_name = stem.lower()
-    stem_num = s + 1
-    for p, profile in enumerate(profiles):
-        profile_name = profile.lower()
-        profile_num = p + 1
+try:
+    os.makedirs('stl')
+except:
+    pass
+
+for stem in stems:
+    for profile in profiles:
         for size in sizes:
             for bool in bools:
                 for b in [0, 1]:
                     bool_name = '_%s' % bool if b else ''
                     cmd = 'openscad '
-                    cmd += '-o stl/%s_%s_x%s%s.stl ' % (stem_name, profile_name, size, bool_name)
-                    cmd += '-D key_type=%d ' % profile_num
-                    cmd += '-D stem_type=%d ' % stem_num
+                    cmd += '-o stl/%s_%s_x%s%s.stl ' % (stem, profile, size, bool_name)
+                    cmd += '-D key_type=%d ' % profiles[profile]
+                    cmd += '-D stem_type=%d ' % stems[stem]
                     cmd += '-D key_size=%s ' % size
                     cmd += '-D %s=%d ' % (bool, b)
                     cmd += 'keycap.scad'
-                    cmds.append(cmd)
                     print(cmd)
                     check_call(cmd, shell=True)
