6db7846a92f5733a60c4e482fae5850df35d57fd
Firstname Lastname <Somone@someplace.com>
2014-12-26 18:59:54 +0000

    Added labelled caps.

--- keycap/keycap_alps_dxf.py
+++ keycap/keycap_alps_dxf.py
@@ -3,6 +3,7 @@
 from dxfwrite import DXFEngine as dxf
 from ndim import *
 import os
+from copy import *
 
 # {{{ Stems
 
@@ -117,9 +118,6 @@ cap_10_x = cap_x*1.0
 cap_15_x = cap_x*1.5
 cap_20_x = cap_x*2.0
 
-# Thickness may be changed as long as nipple_h is changed accordingly.
-d = dxf.drawing('dxf/alps_caps_3mm.dxf')
-
 # Receiver hole for stem nipple.
 recv_x = nipple_w - 0.25
 recv_y = 1.75
@@ -191,6 +189,9 @@ edge.append(edge[0])
 
 # {{{ Caps
 
+# Thickness may be changed as long as nipple_h is changed accordingly.
+d = dxf.drawing('dxf/alps_caps_3mm.dxf')
+
 # Block definition
 cap_10 = dxf.block(name='cap_10')
 this_L = -cap_10_x/2
@@ -253,6 +254,127 @@ d.add( dxf.polyline(edge) )
 d.save()
 
 # }}} Caps
+
+# {{{ Labelled
+
+labelsL = [
+ ('bkspc',  0, -5.4, +4.0),
+ ('~L1',    0, -3.0, +4.0),
+ ('Tab',    0, -3.5, +4.0),
+ ('Esc',    0, -3.0, +4.0),
+ ('Home',   90, -8.5, -4.0),
+ ('End',    90, -8.5, -2.8),
+ ('1',      0, -0.8, +3.0),
+ ('2',      0, -0.8, +3.0),
+ ('3',      0, -0.8, +3.0),
+ ('4',      0, -0.8, +3.0),
+ ('5',      0, -0.8, +3.0),
+ ('F4',     0, -1.6, +3.0),
+ ('\'',     0, -0.3, +3.0),
+ ('<',      0, -0.8, +3.0),
+ ('>',      0, -0.8, +3.0),
+ ('P',      0, -0.8, +3.0),
+ ('Y',      0, -0.8, +3.0),
+ ('A',      0, -0.8, +3.0),
+ ('O',      0, -0.8, +3.0),
+ ('E',      0, -0.8, +3.0),
+ ('U',      0, -0.8, +3.0),
+ ('I',      0, -0.8, +3.0),
+ (':',      0, -0.0, +3.0),
+ ('Q',      0, -0.8, +3.0),
+ ('J',      0, -0.8, +3.0),
+ ('K',      0, -0.8, +3.0),
+ ('X',      0, -0.8, +3.0),
+ ('PgDn',   0, -4.3, +3.0),
+ ('PgUp',   0, -4.8, +3.0),
+ None, # Unused
+ None, # Unused
+ None, # Unused
+ ('`',      0, -0.5, +2.5),
+ ('Alt',    0, -3.0, +3.0),
+ ('Del',    0, -3.0, +3.0),
+ ('~L2',    0, -3.0, +3.0),
+ None, # Spare
+ None, # Spare
+ ('Shift',  90, -13.0, -4.7),
+ ('Ctrl',   90, -13.0, -3.5),
+]
+
+labelsR = [
+ ('Ctrl',   0,      -3.5, +3.0),
+ ('-',      0,      -0.8, +3.0),
+ ('=',      0,      -0.8, +3.0),
+ ('/',      0,      -0.8, +3.0),
+ ('[',      90,     -7.8, +0.0),
+ (']',      90,     -7.8, +0.0),
+ ('6',      0,      -0.8, +3.0),
+ ('7',      0,      -0.8, +3.0),
+ ('8',      0,      -0.8, +3.0),
+ ('9',      0,      -0.8, +3.0),
+ ('0',      0,      -0.8, +3.0),
+ ('F5',     0,      -0.8, +3.0),
+ ('F',      0,      -0.8, +3.0),
+ ('G',      0,      -0.8, +3.0),
+ ('C',      0,      -0.8, +3.0),
+ ('R',      0,      -0.8, +3.0),
+ ('L',      0,      -0.8, +3.0),
+ ('D',      0,      -0.8, +3.0),
+ ('H',      0,      -0.8, +3.0),
+ ('T',      0,      -0.8, +3.0),
+ ('N',      0,      -0.8, +3.0),
+ ('S',      0,      -0.8, +3.0),
+ ('B',      0,      -0.8, +3.0),
+ ('M',      0,      -0.8, +3.0),
+ ('W',      0,      -0.8, +3.0),
+ ('V',      0,      -0.8, +3.0),
+ ('Z',      0,      -0.8, +3.0),
+ ('Right',  0,      -4.3, +3.0),
+ ('Left',   0,      -3.8, +3.0),
+ ('Dn',     0,      -1.8, +3.0),
+ ('Up',     0,      -1.8, +3.0),
+ ('\\',     0,      -0.8, +3.0),
+ ('Mute',   0,      -4.8, +3.0),
+ ('Super',  0,      -5.5, +3.0),
+ ('Shift',  0,      -4.8, +3.0),
+ ('~L2',    0,      -3.0, +3.0),
+ None, # Spare
+ None, # Spare
+ ('Enter',  90,     -13.0, -4.5),
+ ('Space',  90,     -13.0, -4.5),
+]
+
+assert len(pos) == len(labelsL)
+assert len(pos) == len(labelsR)
+
+text = dxf.text('blah', height=3.0, layer='ETCH')
+
+l = deepcopy(d)
+l.filename = 'dxf/alps_caps_mcdoxleft_3mm.dxf'
+l.add_layer('ETCH', color=2)
+for i, label in enumerate(labelsL):
+    if label is None: continue
+    x, y = pos[i][0]
+    t = deepcopy(text)
+    t['text'] = label[0]
+    t['rotation'] = label[1]
+    t['insert'] = (label[2] + x, label[3] + y)
+    l.add(t)
+l.save()
+
+l = deepcopy(d)
+l.filename = 'dxf/alps_caps_mcdoxright_3mm.dxf'
+l.add_layer('ETCH', color=2)
+for i, label in enumerate(labelsR):
+    if label is None: continue
+    x, y = pos[i][0]
+    t = deepcopy(text)
+    t['text'] = label[0]
+    t['rotation'] = label[1]
+    t['insert'] = (label[2] + x, label[3] + y)
+    l.add(t)
+l.save()
+
+# }}} Labelled
 
 # {{{ Cardpack
 
